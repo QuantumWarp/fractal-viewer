c3e606ca8de60714e0ce2279a528585c24a46af5
John Lowther <jw.lowther@hotmail.co.uk>
2022-02-14 22:45:31 +0000

    Fix list and add some defaults

--- src/app/fractal-list-modal/fractal-list.component.ts
+++ src/app/fractal-list-modal/fractal-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Coordinate } from '../../worker/app-workers/shared/coordinate';
+import { ColorSchemeFactory } from '../color-schemes/color-scheme-factory';
 import { FractalListService } from '../services/fractal-list.service';
 import FractalSettings from '../services/fractal-settings';
 import { FractalSettingsService } from '../services/fractal-settings.service';
@@ -20,8 +22,15 @@ export class FractalListComponent {
     public fractalListService: FractalListService,
   ) { }
 
-  load({ settings }): void {
-    this.fractalSettingsService.setSettings(settings);
+  load({ settings }: { settings: FractalSettings }): void {
+    const settingsClone: FractalSettings = {
+      ...settings,
+      params: { ...settings.params },
+      colorScheme: ColorSchemeFactory.create(settings.colorScheme.type),
+      center: new Coordinate(settings.center.x, settings.center.y),
+    };
+
+    this.fractalSettingsService.setSettings(settingsClone);
   }
 
   deleteItem({ id }): void {
--- src/app/services/fractal-list.service.ts
+++ src/app/services/fractal-list.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
+import { BurningShipParams } from '../../worker/app-workers/fractals/burning-ship-params';
+import { MandelbrotSetParams } from '../../worker/app-workers/fractals/mandelbrot-set-params';
 import { Coordinate } from '../../worker/app-workers/shared/coordinate';
 import { ColorSchemeFactory } from '../color-schemes/color-scheme-factory';
+import { Greenscale } from '../color-schemes/schemes/greenscale';
 import FractalSettings from './fractal-settings';
 
 @Injectable()
 export class FractalListService {
-  items: { id: string, name: string, settings: FractalSettings }[] = [];
+  items: {
+    id: string,
+    name: string,
+    settings: FractalSettings,
+  }[] = [];
 
   constructor() {
-    const listString = localStorage.getItem('fractals') || '[]';
-    this.items = JSON.parse(listString, this.jsonReviver);
+    this.load();
   }
 
   save(name: string, settings: FractalSettings): void {
@@ -27,5 +33,47 @@ export class FractalListService {
     if (key === 'center') { return new Coordinate(value.x, value.y); }
     if (key === 'colorScheme') { return ColorSchemeFactory.create(value.type); }
     return value;
+  }
+
+  private load(): string {
+    const listString = localStorage.getItem('fractals');
+    if (!listString) {
+      this.setDefaults();
+    } else {
+      try {
+        this.items = JSON.parse(listString, this.jsonReviver);
+        return;
+      } catch {
+        this.setDefaults();
+      }
+    }
+  }
+
+  private setDefaults(): void {
+    this.items = [{
+      id: '1',
+      name: 'The Ship',
+      settings: {
+        params: new BurningShipParams(100, 2),
+        zoom: 32,
+        zoomFactor: 2,
+        center: new Coordinate(-1.7546875, -0.02703125),
+        colorScheme: new Greenscale(),
+        minColorValue: 0,
+      },
+    },
+    {
+      id: '2',
+      name: 'Self Similar',
+      settings: {
+        params: new MandelbrotSetParams(150, 2),
+        zoom: 128,
+        zoomFactor: 2,
+        center: new Coordinate(-0.128125, -0.987734375),
+        colorScheme: new Greenscale(),
+        minColorValue: 10,
+      },
+    }];
+    localStorage.setItem('fractals', JSON.stringify(this.items));
   }
 }
